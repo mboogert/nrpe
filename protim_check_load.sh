1641e470229cea3c3fefe5396b9cbad3cafbdd12
Marcel Boogert <marcel@mtdb.nl>
2018-01-10 12:22:11 +0100

    Create protim_check_load.sh

---
+++ protim_check_load.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+CHECK_LOAD="/usr/local/nagios/libexec/check_load"
+
+CPU_COUNT="$(grep processor /proc/cpuinfo | wc -l)"
+
+OPTIMAL_LOAD_WARNING="$(echo "$(( CPU_COUNT*3 )),$(( CPU_COUNT*2 )),$(( CPU_COUNT*1 ))")"
+OPTIMAL_LOAD_CRITICAL="$(echo "$(( CPU_COUNT*6 )),$(( CPU_COUNT*4 )),$(( CPU_COUNT*2 ))")"
+
+CHECK_OUTPUT="$($CHECK_LOAD -w "$OPTIMAL_LOAD_WARNING" -c "$OPTIMAL_LOAD_CRITICAL")"
+RETURN_CODE="$?"
+
+if [ "$RETURN_CODE" -gt "0" ]
+then
+  TOP_OUTPUT="$(top -bn 1 | grep -A10 ' PID')"
+  echo "$CHECK_OUTPUT"
+  echo "$TOP_OUTPUT"
+  exit $RETURN_CODE
+else
+  echo $CHECK_OUTPUT
+  exit $RETURN_CODE
+fi
