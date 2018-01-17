0849760d186b1d9eda229ec3a7882140ccf5af47
Marcel Boogert <marcel@mtdb.nl>
2018-01-17 21:33:09 +0100

    Update protam_check_apache.sh

--- protam_check_apache.sh
+++ protam_check_apache.sh
@@ -3,10 +3,10 @@
 #CHECK_PROCS="/usr/lib64/nagios/plugins/check_procs"
 CHECK_PROCS="/usr/local/nagios/libexec/check_procs"
 
-# Get apache server mpm
+# Get apache server MPM (Multi-Processing Module)
 SERVER_MPM="$(httpd -V 2>&1 | grep "Server MPM" | sed 's/^.*://' | sed 's/^[ \t]*//;s/[ \t]*$//')"
 
-# Get serverlimit configuration file
+# Get serverlimit from configuration file
 CONFIGFILE="$(grep -i -r --include \*.conf -e "serverlimit" /etc/httpd/ | grep -v '^.*:.*#' | sed 's/:.*//')"
 if [ "$CONFIGFILE" = "" ]
 then
@@ -16,13 +16,19 @@ else
   SERVERLIMIT="$(echo "$PREFORK_CONFIG" | grep -v "^.*#" | grep -i serverlimit | tr '[:upper:]' '[:lower:]' | sed 's/^.*serverlimit.*[ \t]//')"
 fi
 
+# Get the average httpd memory footprint
 MEMORYFOOTPRINT="$(ps --no-headers -o "rss,cmd" -C httpd | awk '{ sum+=$1 } END { printf ("%d%s\n", sum/NR/1024,"M") }')"
+
+# Get the systems memory size
 SYSTEMMEMORY="$(grep MemTotal /proc/meminfo | sed 's/^.*://' | sed 's/^[ \t]*//;s/[ \t]*$//' | sed 's/ .*$//')"
 SYSTEMMEMORYMB="$(echo $((SYSTEMMEMORY/1024)))"
+
+# Calculate the adviced maximum memory assignment for apache,
+#     calculation: (system memory - 1GB) / average apache memory + 10%
 SERVERLIMIT_ADVICE_RAW="$(awk -vp=$SYSTEMMEMORYMB -vq=$MEMORYFOOTPRINT -vr=1024 'BEGIN{printf "%.0f" ,(p - r) / q}')"
 SERVERLIMIT_ADVICE="$(awk -vp=$SERVERLIMIT_ADVICE_RAW -vq=1.10 'BEGIN{printf "%.0f" ,p * q}')"
 
-# Dynamically set warning and critical tresholds
+# Dynamically set warning and critical tresholds from configured serverlimit directive
 HTTPD_WARNING="$(awk -vp=$SERVERLIMIT -vq=0.90 'BEGIN{printf "%.0f" ,p * q}')"
 HTTPD_CRITICAL="$SERVERLIMIT"
 
@@ -31,7 +37,7 @@ CHECK_OUTPUT="$($CHECK_PROCS -C httpd -w 1:$HTTPD_WARNING -c 1:$HTTPD_CRITICAL)"
 RETURN_CODE="$?"
 echo "$CHECK_OUTPUT"
 
-# Generate a warning if serverlimit above advice
+# Generate a warning if configured serverlimit above calculated adviced serverlimit
 if [ $SERVERLIMIT -gt $SERVERLIMIT_ADVICE ] && [ $RETURN_CODE = 0 ]
 then
   echo "WARNING - Current ServerLimit($SERVERLIMIT) above adviced maximum ServerLimit +10% ($SERVERLIMIT_ADVICE)"
