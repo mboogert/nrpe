a2790d4ab576820b06ba4ed02e09ab317e4ca423
Marcel Boogert <marcel@mtdb.nl>
2018-01-23 08:03:08 +0100

    Update check_apache.sh

--- check_apache.sh
+++ check_apache.sh
@@ -5,39 +5,56 @@
 # Source: https://github.com/mboogert/nrpe/blob/master/check_apache.sh
 #
 
-#CHECK_PROCS="/usr/lib64/nagios/plugins/check_procs"
 CHECK_PROCS="/usr/local/nagios/libexec/check_procs"
 
 # Get apache server MPM (Multi-Processing Module)
-SERVER_MPM="$(httpd -V 2>&1 | grep "Server MPM" | sed 's/^.*://' | sed 's/^[ \t]*//;s/[ \t]*$//')"
-
-# Get serverlimit from configuration file
-CONFIGFILE="$(grep -i -r --include \*.conf -e "serverlimit" /etc/httpd/ | grep -v '^.*:.*#' | sed 's/:.*//' | uniq)"
-if [ "$CONFIGFILE" = "" ]
-then
-  SERVERLIMIT=256
-else
-  PREFORK_CONFIG="$(sed -n -e '/<IfModule mpm_prefork_module>/I,/<\/IfModule>/I p;/<IfModule prefork.c>/I,/<\/IfModule>/I p' $CONFIGFILE | uniq)"
-  SERVERLIMIT="$(echo "$PREFORK_CONFIG" | grep -v "^.*#" | grep -i serverlimit | tr '[:upper:]' '[:lower:]' | sed 's/^.*serverlimit.*[ \t]//')"
-fi
-
-# Get the average httpd memory footprint
-MEMORYFOOTPRINT="$(ps --no-headers -o "rss,cmd" -C httpd | awk '{ sum+=$1 } END { printf ("%d%s\n", sum/NR/1024,"M") }' || ps --no-headers -o "rss,cmd" -C /usr/sbin/httpd | awk '{ sum+=$1 } END { printf ("%d%s\n", sum/NR/1024,"M") }')"
+SERVER_MPM="$(httpd -V 2>&1 | grep "Server MPM" | sed 's/^.*://' | sed 's/^[ \t]*//;s/[ \t]*$//' | tr '[:upper:]' '[:lower:]')"
 
 # Get the systems memory size
 SYSTEMMEMORY="$(grep MemTotal /proc/meminfo | sed 's/^.*://' | sed 's/^[ \t]*//;s/[ \t]*$//' | sed 's/ .*$//')"
 SYSTEMMEMORYMB="$(echo $((SYSTEMMEMORY/1024)))"
+    
+case "$SERVER_MPM" in
+  "worker")
+    # Get the average httpd memory footprint
+    MEMORYFOOTPRINT="$(ps --no-headers -o "rss,cmd" -C httpd | awk '{ sum+=$1 } END { printf ("%d%s\n", sum/NR/1024,"M") }' || ps --no-headers -o "rss,cmd" -C /usr/sbin/httpd | awk '{ sum+=$1 } END { printf ("%d%s\n", sum/NR/1024,"M") }')"
 
-# Calculate the adviced maximum memory assignment for apache,
-#     calculation: (system memory - 1GB) / average apache memory + 10%
-if [ $SYSTEMMEMORYMB -lt 1024 ] 
-then
-  SERVERLIMIT_ADVICE_RAW="$(awk -vp=$SYSTEMMEMORYMB -vq=$MEMORYFOOTPRINT -vr=512 'BEGIN{printf "%.0f" ,(p - r) / q}')"
-else
-  SERVERLIMIT_ADVICE_RAW="$(awk -vp=$SYSTEMMEMORYMB -vq=$MEMORYFOOTPRINT -vr=1024 'BEGIN{printf "%.0f" ,(p - r) / q}')"
-fi
-SERVERLIMIT_ADVICE="$(awk -vp=$SERVERLIMIT_ADVICE_RAW -vq=1.10 'BEGIN{printf "%.0f" ,p * q}')"
+    # Get serverlimit from configuration file
+    CONFIGFILE="$(grep -i -r --include \*.conf -e "serverlimit" /etc/httpd/ | grep -v '^.*:.*#' | sed 's/:.*//' | uniq)"
+    if [ "$CONFIGFILE" = "" ]
+    then
+      SERVERLIMIT=256
+    else
+      PREFORK_CONFIG="$(sed -n -e '/<IfModule mpm_prefork_module>/I,/<\/IfModule>/I p;/<IfModule prefork.c>/I,/<\/IfModule>/I p' $CONFIGFILE | uniq)"
+      SERVERLIMIT="$(echo "$PREFORK_CONFIG" | grep -v "^.*#" | grep -i serverlimit | tr '[:upper:]' '[:lower:]' | sed 's/^.*serverlimit.*[ \t]//')"
+    fi
+    
+    # Calculate the adviced maximum memory assignment for apache,
+    #     calculation: (system memory - 1GB) / average apache memory + 10%
+    if [ $SYSTEMMEMORYMB -lt 1024 ] 
+    then
+      SERVERLIMIT_ADVICE_RAW="$(awk -vp=$SYSTEMMEMORYMB -vq=$MEMORYFOOTPRINT -vr=512 'BEGIN{printf "%.0f" ,(p - r) / q}')"
+    else
+      SERVERLIMIT_ADVICE_RAW="$(awk -vp=$SYSTEMMEMORYMB -vq=$MEMORYFOOTPRINT -vr=1024 'BEGIN{printf "%.0f" ,(p - r) / q}')"
+    fi
+    SERVERLIMIT_ADVICE="$(awk -vp=$SERVERLIMIT_ADVICE_RAW -vq=1.10 'BEGIN{printf "%.0f" ,p * q}')"
+    ;;
 
+  "event")
+    MEMORYFOOTPRINT="1"
+    SERVERLIMIT="256"
+    SERVERLIMIT_ADVICE="256"
+    ;;
+
+  *)
+    SERVER_MPM="<access denied, using default values>"
+    MEMORYFOOTPRINT="1"
+    SERVERLIMIT="256"
+    SERVERLIMIT_ADVICE="256"
+    ;;
+    
+esac
+    
 # Dynamically set warning and critical tresholds from configured serverlimit directive
 HTTPD_WARNING="$(awk -vp=$SERVERLIMIT -vq=0.90 'BEGIN{printf "%.0f" ,p * q}')"
 HTTPD_CRITICAL="$SERVERLIMIT"
