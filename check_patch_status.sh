e7d3f6072e35593d22693880d08ea4d97797cf3b
Marcel Boogert <marcel@mtdb.nl>
2018-02-09 08:58:16 +0100

    Update check_patch_status.sh

--- check_patch_status.sh
+++ check_patch_status.sh
@@ -5,8 +5,9 @@
 # Source: https://github.com/mboogert/nrpe/blob/master/check_patch_status.sh
 #
 
-patch_status_status="$(cat /var/tmp/patch_status | sed 's/|.*$//')"
-patch_status_message="$(cat /var/tmp/patch_status | sed 's/^.*|//')"
+patch_status_location="/var/run/patch_status"
+patch_status_status="$(cat $patch_status_location | sed 's/|.*$//')"
+patch_status_message="$(cat $patch_status_location | sed 's/^.*|//')"
 
 case "$patch_status_status" in
 
