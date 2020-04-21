e9fd77eb8862cb38f0b08251c3abb1dbec406159
Marcel Boogert <marcel@mtdb.nl>
2020-04-21 09:24:11 +0200

    Create check_ntp.sh

---
+++ check_ntp.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+#
+# Author: Marcel Boogert
+# Source: https://github.com/mboogert/nrpe/blob/master/check_ntp.sh
+#
+
+# define static variables
+WARNING="0.5"
+CRITICAL="1"
+JWARN="1:100"
+JCRIT="1:200"
+DELAY="1"
+TIMEOUT=5
+IPVERSION=4
+NTPCONF="/etc/ntp.conf"
+
+# get check_ntp binary
+if [ -x /usr/lib64/nagios/plugins/check_ntp ]; then
+  CHECK_NTP="/usr/lib64/nagios/plugins/check_ntp"
+elif [ -x /usr/local/nagios/libexec/check_ntp ]; then
+  CHECK_NTP="/usr/local/nagios/libexec/check_ntp"
+else
+  echo "UNKNOWN: cannot find a suitable check_ntp binary"
+  exit 3
+fi
+
+# declare an array
+declare -A arr_retout=()
+declare -A arr_retval=()
+declare -A arr_status_ok=()
+
+status_ok=0
+status_warning=0
+status_critical=0
+status_unknown=0
+
+# get configured ntp servers
+for SERVER in `grep -e '^server *' $NTPCONF | sed 's/^.*server //' | sed 's/ .*$//'`; do
+  arr_retout[$SERVER]="$($CHECK_NTP --host=${SERVER} -${IPVERSION} --warning=${WARNING} --critical=${CRITICAL} --delay=${DELAY} --timeout=${TIMEOUT})"
+  arr_retval[$SERVER]="$?"
+  case "${arr_retval[$SERVER]}" in
+    0)
+      ((status_ok++))
+      ;;
+    1)
+      ((status_warning++))
+      ;;
+    2)
+      ((status_critical++))
+      ;;
+    3)
+      ((status_unknown++))
+      ;;
+  esac
+done
+
+SERVERCOUNT="${#arr_retval[@]}"
+
+if [ $status_ok -eq $SERVERCOUNT ] || [ $status_ok -gt 1 ]; then
+  echo "OK: Two or more configured NTP servers are responding correctly, server time is OK"
+elif [ $status_ok -eq 1 ]; then
+  echo "WARNING: Only 1 NTP server is responding, server time is OK"
+else
+  echo "CRITICAL: No NTP servers are responding"
+fi
+
+echo ""
+for SERVER in "${!arr_retval[@]}"
+do
+  DATA="$(echo ${arr_retout[$SERVER]} | sed 's/|.*$//')"
+  PERFDATA="$(echo ${arr_retout[$SERVER]} | sed 's/^.*|//')"
+  echo "$SERVER - $DATA | ${SERVER}_${PERFDATA}"
+done
